ff91e827b5c4538ecc68615f6b37f6e43d5fc291
Sehrish Noreen <sehrishnoreen31@gmail.com>
2025-07-14 19:11:37 +0500

    Alter

---
+++ alter.sql
@@ -0,0 +1,3 @@
+-- Active: 1752500983595@@127.0.0.1@5432@college
+alter table student add email CHAR(50);
+-- add new column
---
+++ select.sql
@@ -0,0 +1,2 @@
+select first_name, last_name from student
+where id=5 or id=3;
